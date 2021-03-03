37903697ea19509a05237d186982483de25b591f
josefaidt <josef.aidt@gmail.com>
2021-03-02 18:15:39 -0600

    add pdf script, test api

---
+++ scripts/pdf.js
@@ -0,0 +1,22 @@
+const { spawn } = require('child_process')
+const puppeteer = require('puppeteer')
+
+async function main() {
+  const devServer = spawn('yarn', ['dev'])
+  devServer.stdout.on('data', async data => {
+    const msg = String(data)
+    if (msg.startsWith('ready')) {
+      console.info('Next.js server ready')
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto('http://localhost:3000')
+      console.info('Creating PDF')
+      await page.pdf({ path: 'josefaidt_resume.pdf', format: 'a4' })
+      devServer.kill()
+      await browser.close()
+      console.info('done!')
+    }
+  })
+}
+
+main()
--- src/pages/api/pdf.js
+++ src/pages/api/pdf.js
@@ -1,13 +1,19 @@
 import puppeteer from 'puppeteer'
 
-const handler = async function handler(req, res) {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('http://localhost:5000')
-  await page.pdf({ path: 'josefaidt_resume.pdf', format: 'a4' })
+let handler = function (req, res) {
+  res.send('This route is only for development and testing purposes')
+}
 
-  await browser.close()
-  res.send('ok')
+if (process.env.NODE_ENV !== 'production') {
+  handler = async function handler(req, res) {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('http://localhost:3000')
+    await page.pdf({ path: 'josefaidt_resume.pdf', format: 'a4' })
+
+    await browser.close()
+    res.send('ok')
+  }
 }
 
 export default handler
